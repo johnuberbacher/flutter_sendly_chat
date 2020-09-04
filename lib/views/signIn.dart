0ce2526dbfae49806e3d0b93a37506bb08f3f0d2
John Uberbacher <johnuberbacher@gmail.com>
2020-09-04 09:15:44 -0600

    Started Home/Login Screen development

--- lib/views/signIn.dart
+++ lib/views/signIn.dart
@@ -15,80 +15,122 @@ class _SignInState extends State<SignIn> {
       body: Container(
         width: double.infinity,
         margin: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('assets/icons/handle-chat-logo-shadow.png',
-                width: 250.0),
-            Container(
-              margin: const EdgeInsets.only(
-                bottom: 20.0,
-              ),
-              width: double.infinity,
-              child: RaisedButton(
-                color: Color(0xFFd83256),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(60),
-                ),
-                onPressed: () {},
-                textColor: Colors.white,
-                padding: const EdgeInsets.only(
-                  top: 17.0,
-                  right: 30.0,
-                  bottom: 17.0,
-                  left: 30.0,
-                ),
-                child: const Text(
-                  'SIGN IN',
-                  style: TextStyle(
-                    letterSpacing: 1,
-                    fontSize: 16,
+        child: ScrollConfiguration(
+          behavior: new ScrollBehavior()
+            ..buildViewportChrome(context, null, AxisDirection.down),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/icons/handle-chat-logo-shadow.png',
+                    width: 170.0),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20.0,
+                  ),
+                  width: double.infinity,
+                  child: TextField(
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF373753),
+                      hintText: "email",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(
-                bottom: 40.0,
-              ),
-              width: double.infinity,
-              child: RaisedButton(
-                color: Color(0xFFf3f3f3),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(60),
-                ),
-                onPressed: () {},
-                padding: const EdgeInsets.only(
-                  top: 17.0,
-                  right: 30.0,
-                  bottom: 17.0,
-                  left: 30.0,
-                ),
-                child: const Text(
-                  'SIGN IN WITH GOOGLE',
-                  style: TextStyle(
-                    letterSpacing: 1,
-                    fontSize: 16,
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20.0,
+                  ),
+                  width: double.infinity,
+                  child: TextField(
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF373753),
+                      hintText: "email",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(
-                bottom: 20.0,
-              ),
-              width: double.infinity,
-              child: const Text(
-                "Don't have an account? Register Now",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.white70,
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20.0,
+                  ),
+                  width: double.infinity,
+                  child: RaisedButton(
+                    color: Color(0xFFd83256),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    onPressed: () {},
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.only(
+                      top: 17.0,
+                      right: 30.0,
+                      bottom: 17.0,
+                      left: 30.0,
+                    ),
+                    child: const Text(
+                      'SIGN IN',
+                      style: TextStyle(
+                        letterSpacing: 1,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
                 ),
-                textAlign: TextAlign.center,
-              ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 40.0,
+                  ),
+                  width: double.infinity,
+                  child: RaisedButton(
+                    color: Color(0xFFf3f3f3),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60),
+                    ),
+                    onPressed: () {},
+                    padding: const EdgeInsets.only(
+                      top: 17.0,
+                      right: 30.0,
+                      bottom: 17.0,
+                      left: 30.0,
+                    ),
+                    child: const Text(
+                      'SIGN IN WITH GOOGLE',
+                      style: TextStyle(
+                        letterSpacing: 1,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20.0,
+                  ),
+                  width: double.infinity,
+                  child: const Text(
+                    "Don't have an account? Register Now",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white70,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
