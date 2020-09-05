0fa620990165434aceca519e1989827c0d4a0ab7
John Uberbacher <johnuberbacher@gmail.com>
2020-09-04 21:37:27 -0600

    Firebase connections

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'views/signIn.dart';
+import 'views/signUp.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -16,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SignIn(),
+      home: SignUp(),
     );
   }
 }
--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -63,3 +63,33 @@ InputDecoration textFieldInputDecoration(String hintText) {
     ),
   );
 }
+
+InputDecoration emailTextFieldInputDecoration(String hintText) {
+  return InputDecoration(
+    contentPadding: const EdgeInsets.all(20.0),
+    border: new OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    filled: true,
+    fillColor: Color(0xFF373753),
+    hintText: hintText,
+    prefixIcon: Padding(
+      padding: const EdgeInsets.only(
+        right: 20.0,
+        left: 20.0,
+        bottom: 1.0,
+      ),
+      child: Icon(
+        Icons.mail_outline,
+        color: Color(0xFF1f1e30),
+      ),
+    ),
+    hintStyle: TextStyle(
+      color: Color(0xFFb1b2c4),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white38),
+      borderRadius: BorderRadius.circular(10),
+    ),
+  );
+}
